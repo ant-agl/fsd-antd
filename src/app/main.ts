ffbaa804d40803e800ee706405fb17508c27c5a7
ant-agl <aglanton31@gmail.com>
2024-12-24 20:47:11 +0300

    Add phone mask global

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,9 +4,14 @@ import App from "./App.vue";
 import "ant-design-vue/dist/reset.css";
 import { router } from "./providers/router";
 import { store } from "./providers/store";
+import VueTheMask from "vue-the-mask";
 
 const app = createApp(App);
 
-app.use(Antd).use(router).use(store);
+app
+  .use(Antd)
+  .use(router)
+  .use(store)
+  .use(VueTheMask as any);
 
 app.mount("#app");
